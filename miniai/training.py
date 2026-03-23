29c0847af27d9bf104996c43e5f34eac66871f4a
root <shin.yongwhan23@gmail.com>
2026-03-23 17:26:12 +0000

    syncing notebook with library

--- miniai/training.py
+++ miniai/training.py
@@ -3,28 +3,28 @@
 # %% auto #0
 __all__ = ['accuracy', 'report', 'Dataset', 'fit', 'get_dls']
 
-# %% ../nbs/04_minibatch_training.ipynb #1a00e556
+# %% ../nbs/04_minibatch_training.ipynb #819a1d64
 import pickle,gzip,math,os,time,shutil,torch,matplotlib as mpl,numpy as np,matplotlib.pyplot as plt
 from pathlib import Path
 from torch import tensor,nn
 import torch.nn.functional as F
 
-# %% ../nbs/04_minibatch_training.ipynb #83cc8762
+# %% ../nbs/04_minibatch_training.ipynb #eaef640c
 def accuracy(out, yb): return (out.argmax(dim=1)==yb).float().mean()
 
-# %% ../nbs/04_minibatch_training.ipynb #f5c1f96d
+# %% ../nbs/04_minibatch_training.ipynb #e15f01f4
 def report(loss, preds, yb): print(f'{loss:.2f}, {accuracy(preds, yb):.2f}')
 
-# %% ../nbs/04_minibatch_training.ipynb #4e8a36f7
+# %% ../nbs/04_minibatch_training.ipynb #f2c108e8
 class Dataset():
     def __init__(self, x, y): self.x,self.y = x,y
     def __len__(self): return len(self.x)
     def __getitem__(self, i): return self.x[i],self.y[i]
 
-# %% ../nbs/04_minibatch_training.ipynb #4c642d9e
+# %% ../nbs/04_minibatch_training.ipynb #f0af5956
 from torch.utils.data import DataLoader, SequentialSampler, RandomSampler, BatchSampler
 
-# %% ../nbs/04_minibatch_training.ipynb #e0897aeb
+# %% ../nbs/04_minibatch_training.ipynb #7be1c3f7
 def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
     for epoch in range(epochs):
         model.train()
@@ -46,7 +46,7 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
         print(epoch, tot_loss/count, tot_acc/count)
     return tot_loss/count, tot_acc/count
 
-# %% ../nbs/04_minibatch_training.ipynb #16f8128b
+# %% ../nbs/04_minibatch_training.ipynb #d12f3ee2
 def get_dls(train_ds, valid_ds, bs, **kwargs):
     return (DataLoader(train_ds, batch_size=bs, shuffle=True, **kwargs),
             DataLoader(valid_ds, batch_size=bs*2, **kwargs))
