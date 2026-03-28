c90949b54a5ab0f321cc9c456c74724a89522710
root <shin.yongwhan23@gmail.com>
2026-03-28 03:04:13 +0000

    learner notebook py and conv py? added

--- miniai/conv.py
+++ miniai/conv.py
@@ -3,7 +3,7 @@
 # %% auto #0
 __all__ = ['def_device', 'conv', 'to_device', 'collate_device']
 
-# %% ../nbs/07_convolutions.ipynb #f3f60ab0
+# %% ../nbs/07_convolutions.ipynb #c7a330e2
 import torch
 from torch import nn
 
@@ -13,13 +13,13 @@ from typing import Mapping
 from .training import *
 from .datasets import *
 
-# %% ../nbs/07_convolutions.ipynb #aa0c1570
+# %% ../nbs/07_convolutions.ipynb #0a68cc3b
 def conv(ni, nf, ks=3, stride=2, act=True):
     res = nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2)
     if act: res = nn.Sequential(res, nn.ReLU())
     return res
 
-# %% ../nbs/07_convolutions.ipynb #169e7de4
+# %% ../nbs/07_convolutions.ipynb #1d84b569
 def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def to_device(x, device=def_device):
